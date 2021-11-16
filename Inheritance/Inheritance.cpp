5c8eeb9339020098301639e9cabc96a5f7e2d47a
yandutka@mail.ru <yandutka@mail.ru>
2021-11-16 21:45:02 +0300

    Add project files.

---
+++ Inheritance/Inheritance.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Human
+{
+	string last_name;
+	string first_name;
+	unsigned int age;
+public:
+	const string& get_last_name()const
+	{
+		return last_name;
+	}
+	const string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+
+	//            Constructors:
+	Human(const string& last_name, const string& first_name, unsigned int age)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	//             Methods:
+	void print()const
+	{
+		cout << last_name << " " << first_name << " " << age << "лет.\n";
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Human h("Montana", "Antonio", 35);
+	h.print();
+}
