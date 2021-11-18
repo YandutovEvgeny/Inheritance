9087b59e31e1a2881d53826f2c1d7c9ddc1e0041
yandutka@mail.ru <yandutka@mail.ru>
2021-11-18 20:58:41 +0300

    Polymorphism

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -47,7 +47,7 @@ public:
 	}
 
 	//             Methods:
-	void print()const
+	virtual void print()const
 	{
 		cout << last_name << " " << first_name << " " << age << "лет.\n";
 	}
@@ -210,9 +210,12 @@ public:
 	}
 };
 
+//#define INHERITANCE
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef INHERITANCE
 	Human t("Montana", "Antonio", 35);
 	t.print();
 
@@ -224,4 +227,23 @@ void main()
 
 	Graduate i("Savchenko", "Oleg", 30, "C++ developer", "WW_01", 93, "Разработка ПО для коммерческих компаний", 12);
 	i.print();
+#endif // INHERITANCE
+
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_01", 93),  //upcast
+		new Student("Vercetti", "Tomas", 30, "Cryminal", "Vice", 90),    //upcast
+		new Teacher("Сащеко", "Роман", 33, "C++ basic and OOP teacher", 10, "good"),  //upcast
+		new Student("Diaz", "Ricardo", 55, "Weapon distribution", "Vice", 80),
+		new Graduate("Savchenko", "Oleg", 30, "C++ developer", "WW_01", 93,
+		"Разработка ПО для коммерческих компаний", 12),
+		new Teacher("Einstein", "Albert", 143, "Astronomy", 120, "good")
+	};
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		cout << "\n------------------------------\n";
+		group[i]->print();
+	}
+	cout << "\n------------------------------\n";
 }
