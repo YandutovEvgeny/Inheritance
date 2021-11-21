a666066e211d41e32367f29fe34ef828dcd7c58e
yandutka@mail.ru <yandutka@mail.ru>
2021-11-21 19:52:26 +0300

    commit

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,9 +1,10 @@
-﻿#include"Human.h"
+﻿#include<fstream>
+#include"Human.h"
 #include"Student.h"
 #include"Teacher.h"
 #include"Graduate.h"
 
-#define INHERITANCE
+//#define INHERITANCE
 
 void main()
 {
@@ -18,22 +19,22 @@ void main()
 	Teacher s("Сащеко", "Роман", 33, "C++ basic and OOP teacher", 10, "good");
 	s.print();
 
-	Graduate i("Savchenko", "Oleg", 30, "C++ developer", "WW_01", 93, "Разработка ПО для коммерческих компаний", 12);
+	Graduate i("Savchenko", "Oleg", 30, "C++ developer", "WW_01", 93, "Develop software for inc.", 12);
 	i.print();
 #endif // INHERITANCE
 
-	/*Human* group[] =
+	Human* group[] =
 	{
 		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_01", 93),  //upcast
 		new Student("Vercetti", "Tomas", 30, "Cryminal", "Vice", 90),    //upcast
 		new Teacher("Сащеко", "Роман", 33, "C++ basic and OOP teacher", 10, "good"),  //upcast
 		new Student("Diaz", "Ricardo", 55, "Weapon distribution", "Vice", 80),
 		new Graduate("Savchenko", "Oleg", 30, "C++ developer", "WW_01", 93,
-		"Разработка ПО для коммерческих компаний", 12),
+		"Develop software for inc.", 12),
 		new Teacher("Eistein", "Albert", 143, "Astronomy", 120, "good")
 	};
 
-	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	/*for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
 	{
 		cout << "\n------------------------------\n";
 		group[i]->print();
@@ -44,4 +45,32 @@ void main()
 	{
 		delete[] group[i];
 	}*/
+
+
+	fstream fout("File.txt", std::ios_base::app);
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		//fout.write((char*)&group, (sizeof(group) / sizeof(Human*)));
+		fout << group[i];
+	}
+	fout.close();
+	system("notepad File.txt");
+
+	const int SIZE = 256;
+	char buffer[SIZE]{};
+	ifstream fin("File.txt");
+	if (fin.is_open())
+	{
+
+		while (!fin.eof())
+		{
+			fin.getline(buffer, SIZE);
+			cout << buffer << endl;
+		}
+	}
+	else
+	{
+		cerr << "File not found" << endl;
+	}
+	fin.close();
 }
--- Files/Main.cpp
+++ Files/Main.cpp
@@ -2,19 +2,21 @@
 #include<fstream>
 using namespace std;
 
-//#define WRITE_TO_FILE
+#define WRITE_TO_FILE
+//#define READ_FROM_FILE
 
 void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef WRITE_TO_FILE
 	//cout << "Hello World" << endl;
-	ofstream fout("File.docx", std::ios_base::app/*append*/);   //Создаём и открываем поток
+	ofstream fout("File.txt", std::ios_base::app/*append*/);   //Создаём и открываем поток
 	fout << "Hello World" << endl;
 	fout.close();
 	system("notepad File.txt");
 #endif // WRITE_TO_FILE
 
+#ifdef READ_FROM_FILE
 	const int SIZE = 256;
 	char buffer[SIZE]{};
 
@@ -33,4 +35,6 @@ void main()
 		cerr << "File not found" << endl;
 	}
 	fin.close();
+#endif // READ_FROM_FILE
+
 }
